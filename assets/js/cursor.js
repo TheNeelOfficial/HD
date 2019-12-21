d5e4123e0df912d7dde5c10f70455520edb5c1dd
neelakanteswar <patnaikneelakanteswar@gmail.com>
2019-12-22 04:43:53 +1100

    home page created

---
+++ assets/js/cursor.js
@@ -0,0 +1,30 @@
+var cursor = document.getElementById('custom-cursor');
+var cursorDot = document.getElementById('custom-cursor-dot');
+
+document.addEventListener("mousemove",function(event){
+
+  var x = event.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
+  var y = event.clientY + document.body.scrollTop + document.documentElement.scrollTop;
+
+  cursor.style.left = x + 'px';
+  cursor.style.top  = y + 'px';
+  cursorDot.style.left = x + 'px';
+  cursorDot.style.top  = y + 'px';
+
+});
+
+var links = document.getElementsByTagName('a');
+for(var i = 0, len = links.length; i < len; i++) {
+    links[i].onmouseenter = function(){mouseOver()};
+    links[i].onmouseleave = function(){mouseOut()};
+}
+
+function mouseOver() {
+  cursor.classList.add("hovering-link");
+  cursorDot.classList.add("hovering-link");
+}
+
+function mouseOut() {
+  cursor.classList.remove("hovering-link");
+  cursorDot.classList.remove("hovering-link");
+}
