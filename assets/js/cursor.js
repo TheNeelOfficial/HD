44105c3f59f63f9b6576608d46e3332581353a0e
neelakanteswar <patnaikneelakanteswar@gmail.com>
2020-01-07 20:22:20 +0530

    added changes for hosting

--- assets/js/cursor.js
+++ assets/js/cursor.js
@@ -1,3 +1,5 @@
+// home page cursor
+
 var cursor = document.getElementById('custom-cursor');
 var cursorDot = document.getElementById('custom-cursor-dot');
 
@@ -27,4 +29,39 @@ function mouseOver() {
 function mouseOut() {
   cursor.classList.remove("hovering-link");
   cursorDot.classList.remove("hovering-link");
+}
+
+
+
+// other pages cursor
+
+var other_cursor = document.getElementById('other_custom-cursor');
+var other_cursorDot = document.getElementById('other_custom-cursor-dot');
+
+document.addEventListener("mousemove",function(event){
+
+  var x = event.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
+  var y = event.clientY + document.body.scrollTop + document.documentElement.scrollTop;
+
+  other_cursor.style.left = x + 'px';
+  other_cursor.style.top  = y + 'px';
+  other_cursorDot.style.left = x + 'px';
+  other_cursorDot.style.top  = y + 'px';
+
+});
+
+var links = document.getElementsByTagName('a');
+for(var i = 0, len = links.length; i < len; i++) {
+    links[i].onmouseenter = function(){mouseOver()};
+    links[i].onmouseleave = function(){mouseOut()};
+}
+
+function mouseOver() {
+  other_cursor.classList.add("hovering-link");
+  other_cursorDot.classList.add("hovering-link");
+}
+
+function mouseOut() {
+  other_cursor.classList.remove("hovering-link");
+  other_cursorDot.classList.remove("hovering-link");
 }
